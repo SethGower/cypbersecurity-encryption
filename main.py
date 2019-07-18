cb0a920da17292f640ffd7880a6614778ee7bea9
Seth Gower <sdg7234@rit.edu>
2019-07-18 11:14:45 -0400

    inital commit 2: electric boogaloo

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from ciphers import *
 def main():
-    message = input("Please input a message to be encrypted")
-    keyword = input("Please input a codeword")
+    message = input("Please input a message to be encrypted: ")
+    keyword = input("Please input a codeword: ")
     encrypted = xor(message, keyword)
+    print(len(encrypted))
     print(encrypted)
     decrypted = xor(encrypted, keyword)
     print(decrypted)
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Seth's Encryption Course
+
+Since the provided course material isn't great for a lot of the course, I have
+written some of my own stuff. Most of my functions are located in the
+`ciphers.py` file, which can just be imported as a library, as I do in
+`main.py`. I will be adding more, and if you have other things or questions,
+please feel free to make issues or even pull requests.
